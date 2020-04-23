034d21d1b25e6ab4122d3518f990485544caaadf
Bhavika007 <57570012+Bhavika007@users.noreply.github.com>
2020-04-23 11:46:24 +0530

    Update RemoveHyphen.java

--- RemoveHyphen.java
+++ RemoveHyphen.java
@@ -1,18 +1,30 @@
 package com.tgt.igniteplus;
 import java.util.*;
-public class RemoveHyphen { public static void main(String[] args) {
-    String str;
-    int j=0;
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter the string");
-    str = in.nextLine();
-    char array[]=str.toCharArray();
-    char temp[]=new char[str.length()];
-    for (int i = 0; i <str.length() ; i++) {
-        if (array[i] == '-' && Character.isDigit(array[i - 1]) && Character.isDigit(array[i + 1])) ;
-        else
-            temp[j++] = array[i];
-    }
+public class RemoveHyphen { 
+    public static void main(String[] args) {
+    
+     Scanner in = new Scanner(System.in);
+   
+     System.out.println("Enter the string: ");
+    
+     String str;
+     str = in.nextLine();
+    
+     char array[]=str.toCharArray();
+   
+     char temp[]=new char[str.length()];
+   
+        int j = 0;
+   
+        for (int i = 0; i <str.length() ; i++) {
+       
+            if (array[i] == '-' && Character.isDigit(array[i - 1]) && Character.isDigit(array[i + 1])) ;
+       
+            else
+           
+                temp[j++] = array[i];
+   
+       }
     System.out.println(temp);
 
 }
